fd11df1edb7e2c613eaa2c75fab06942e7a46068
giaphu20056 <giaphu280605@gmail.com>
2025-03-22 21:08:45 +0700

    fix login.php

--- login.php
+++ login.php
@@ -22,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="style/style.css">
   <title>Admin Login</title>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
   
